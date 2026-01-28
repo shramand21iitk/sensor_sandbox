9bf772d13558211d6f4891bda293c07854ec9b82
shramand21 <shramand21@iitk.ac.in>
2026-01-28 13:43:29 +0530

    Second commit

--- pca9685_control.c
+++ pca9685_control.c
@@ -8,27 +8,3 @@
 #include <string.h>
 #include <linux/i2c.h>
 
-#define LED0_ON_L 0x0006
-#define LED3_OFF_H 0x0015
-#define LED15_OFF_H 0x0045
-
-int convert_to_hex(int duty_cycle){
-    int val = duty_cycle*4096/100;
-    return val;
-}
-
-int main(){
-    
-    int file;
-    char filename[20];
-    int adapter_nr = 1;
-    // Check whether the adapter is working or not
-    snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
-    file = open(filename, O_RDWR);
-    if (file<0){
-        perror("I2C bus adapter failed");
-        exit(1);
-    }
-
-    return 0;
-}
